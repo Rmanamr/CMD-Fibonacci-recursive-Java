b4120974593015e698ecc7dbcd9490760cd40def
Arman Azarnik <armanazarnik@gmail.com>
2023-09-03 12:20:27 +0330

    add contact info v 2.1

--- FibonacciRecursive.java
+++ FibonacciRecursive.java
@@ -3,25 +3,27 @@
 import java.util.Scanner;
 
 public class FibonacciRecursive{
-    public static int fib(int n){
-        if(n==1)
-            return 0;
-        else if(n==2)
-            return 1;
-        else
-            return fib(n-1)+fib(n-2);
-    }
     public static void main(String args []){
         while(true){
             System.out.println("Please enter the number of fibonacci series :");
             Scanner input = new Scanner(System.in);
             int numberOfSeries=input.nextInt();
-            System.out.println("*********");
+            System.out.println("**************************");
             for(int i=1; i<=numberOfSeries; i++)
-                System.out.println(fib(i));
+                System.out.println(fibonacciSerieRecursive(i));
         }
-    }   
+    }  
+
+    public static int fibonacciSerieRecursive(int n){
+        if(n == 1)
+            return 0;
+        else if(n==2)
+            return 1;
+        else
+            return fibonacciSerieRecursive(n-1)+fibonacciSerieRecursive(n-2);
+    } 
 }
 
 
-//by Arman Azarnik
+// by Arman Azarnik
+// armanazarnik@gmail.com
